34bdded034fc5454319c770194308294d802e532
teej043 <teej043@gmail.com>
2015-08-27 03:08:25 +0800

    updates

--- src/js/assets/main.js
+++ src/js/assets/main.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
+	/*
 	var menuItemsNum = ($('.navibar ul.menu > li').length);
-	
+
 	$('.topbar').addClass('nav-full').clone().addClass('nav-mobile').removeClass('nav-full').appendTo( ".head" );
-	
+
 	$('.nav-full ul.menu').addClass('itemsnum-'+menuItemsNum);
-	
+
 	$(".nav-mobile .navibar ul li:not('.drpdwn')").on('click',function(){
 		if (btnlogin.hasClass('active')){
 			btnlogin.removeClass('active');
 		}
 	});
-	
+
 	$('.nav-mobile').stick_in_parent({
 		parent : 'body'
 	});
 
-	
+
 	var btnlogin;
 	btnlogin = $(".nav-mobile .navibar .menu-button").parent().parent();
 	$('.nav-mobile .navibar .menu-button').on('click',function(){
@@ -27,13 +28,13 @@ $(document).ready(function(){
 	btnlogin.click(function(e){
 		e.stopPropagation();
 	});
-	
-	
+
+
 	$(".page").on('click',function(e){
 		$('.navibar ul li').removeClass('active');
 		$('.submenu').fadeOut('fast');
 	});
-	
+
 	$('.navibar ul').on('click','li',function(e){
 		if ($(this).find('.submenu').length){
 			if ($(this).hasClass('active')){
@@ -46,9 +47,10 @@ $(document).ready(function(){
 		}
 		e.stopPropagation();
 	});
-	
-	
-	
+	*/
+
+
+
 	var crsl = $("#frontslides");
 	crsl.owlCarousel({
 		animateOut: 'fadeOut',
@@ -57,26 +59,27 @@ $(document).ready(function(){
 		nav: true,
 		loop:true,
 		lazyLoad:true,
-		items : 1
+		items : 1,
+		mouseDrag: false
 	});
-	
+
 	var datadots = [],i=0;
 	$(crsl).find('.slide').each(function(){
 		datadots.push($(this).data('dot'));
 	});
-	
+
 	$(crsl).find('.owl-dot').each(function(){
 		$(this).html('<span>'+datadots[i]+'</span>');
 		i++;
 	});
-	
+
 	var shcs = $('#showcase');
 	shcs.pajinate({
 		num_page_links_to_display : 5,
 		items_per_page : 1,
 		slideTransition: true
 	});
-	
+
 	$('.popshow').fancybox({
     	helpers : {
     		title : {
@@ -84,17 +87,17 @@ $(document).ready(function(){
     		}
 		}
     });
-	
-	
-	
 
 
-	
-
-	
 
 
-	
 
-	
+
+
+
+
+
+
+
+
 });
